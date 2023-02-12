a8bac65db9926ccb5dd9c649da5e3bcecb2825eb
Antonio Dilone <antoniodilone26@gmail.com>
2023-02-12 12:43:57 -0400

    Agregar archivos de proyecto.

---
+++ Fracciones/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fracciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que convierte fracciones mixtas en fracciones comunes");
+            Console.WriteLine("Ingrese el numero entero");
+            int ent = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el numerador de la fraccion");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el denominador de la fraccion");
+            int den = int.Parse(Console.ReadLine());
+
+            int num2 = (ent * den) + num;
+
+
+            Console.WriteLine("Su nueva fraccion es " + num2 + "/" + den);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Numeros primos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeros_primos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que calcula una lista de numeros primos");
+            Console.WriteLine("Cantidad de numero primos que quiere visualizar");
+            int cant = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cant; i++)
+            {
+                num = Math
+
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
