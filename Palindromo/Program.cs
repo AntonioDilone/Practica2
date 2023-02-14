4fb448897b63b1abb2ef2a9861560ecaee92d164
Antonio Dilone <antoniodilone26@gmail.com>
2023-02-13 21:26:35 -0400

    Palindromo

--- Palindromo/Program.cs
+++ Palindromo/Program.cs
@@ -10,6 +10,25 @@ namespace Palindromo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Programa que analiza un string y determina si es un palindromo");
+            Console.WriteLine("Ingresar una palabra");
+            string palabra = Console.ReadLine();
+            string reverso = "";
+
+            for (int i = palabra.Length - 1; i >=0 ; i--)
+            {
+                reverso += palabra[i];
+            }
+
+            if (palabra == reverso)
+            {
+                Console.WriteLine("La palabra es un palindromo");
+            }
+            else
+            {
+                Console.WriteLine("La palabra no es un palindromo");
+            }
+
         }
     }
 }
