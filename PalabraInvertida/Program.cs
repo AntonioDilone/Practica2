c88a7e92195dd7334dd76b60e11293b8267afe80
Antonio Dilone <antoniodilone26@gmail.com>
2023-02-13 21:35:04 -0400

    Palabra invertida

---
+++ PalabraInvertida/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalabraInvertida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que inverte las palabras");
+            Console.WriteLine("Ingrese una palabra");
+            string palabra = Console.ReadLine();
+            string reverso = "";
+            for (int i = palabra.Length - 1; i >= 0 ; i--)
+            {
+                reverso+= palabra[i];
+            }
+            Console.WriteLine(reverso);
+            Console.ReadLine();
+
+        }
+    }
+}
